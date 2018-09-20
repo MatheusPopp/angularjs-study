3d9fdb40d94f86c8885aad61a2b7fec2acfaea8a
Matheus Popp <matheus.popp@outlook.com>
2018-09-20 12:05:30 -0300

    Built the index page(login) and added login controller and main controller

---
+++ angular-study-app/angular-study-app/www/controller/loginController.js
@@ -0,0 +1,21 @@
+﻿angular.module('myApp')
+    .controller('loginController', ['$scope',function ($scope) {
+
+
+
+        $scope.model = {
+            email: undefined,
+            senha: undefined,
+            lembrar: undefined
+        }
+
+        $scope.lembrarChange = function(){
+            if (!$scope.model.lembrar) {
+                alert("mudou");
+            }
+        }
+
+
+
+
+    }]);
