6a41396de3ea464e33558e62998354f51d0d23f2
Matheus Popp <matheus.popp@outlook.com>
2018-09-24 09:15:47 -0300

    Added a form on cadastro.html and changed cadastroController

--- angular-study-app/angular-study-app/www/controller/cadastroController.js
+++ angular-study-app/angular-study-app/www/controller/cadastroController.js
@@ -2,6 +2,21 @@
     .controller('cadastroController', ['$scope','$location',function ($scope,$location) {
 
 
+        $scope.cadastrar = function () {
+            alert($scope.model.nome + $scope.model.email + $scope.model.genero + $scope.model.data_nasc);;
+        }
+        $scope.sair = function () {
+            $location.url('/');
+        }
+
+        $scope.model = {
+            nome: undefined,
+            email: undefined,
+            genero: "masculino",
+            data_nasc: undefined
+        }
+
+
 
 
 
